7535330a5090f43ced39d46dc1d4bc5631299519
Simon Lee <simonlee0306@gmail.com>
2018-10-21 21:59:32 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-	programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["Lisp", "C"]}
+#	programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["Lisp", "C"]}
+	programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["Lisp", "C"]}
+	programmer_hash[:yukihiro_matsumoto]
 end
 
 def changing_alan
